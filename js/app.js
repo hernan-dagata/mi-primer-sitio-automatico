410a4f7f9204d52bcff39dc0fc0432c682cdc741
Hernan Garcia <hernandagata@gmail.com>
2026-06-20 14:41:31 -0500

    Se finaliza el crud y se ajustan estilos

--- js/app.js
+++ js/app.js
@@ -6,23 +6,48 @@ const supabaseClient = supabase.createClient(
     SUPABASE_KEY
 );
 
+let editandoId = null;
+
+function formatearFecha(fecha) {
+    return new Date(fecha).toLocaleString("es-CO", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric"
+    });
+}
+
 async function cargarUsuarios() {
     const { data, error } = await supabaseClient
         .from("usuarios")
-        .select("*");
+        .select("*")
+        .order("id");
     if (error) {
         console.log("Error:", error);
         return;
     }
     const container = document.getElementById("userContainer");
+    container.innerHTML = "";
     data.forEach(usuario => {
         const card = document.createElement("div");
         card.classList.add("user-card");
         card.innerHTML = `
-        <h3>${usuario.name}</h3>
-        <p>${usuario.email}</p>
-        <small>${usuario.created_at}</small>
-    `;
+            <h3>${usuario.name}</h3>
+            <p>${usuario.email}</p>
+            <small>
+                📅 Registrado el:
+                ${formatearFecha(usuario.created_at)}
+            </small>
+            <button onclick="editarUsuario(
+                '${usuario.id}',
+                '${usuario.name}',
+                '${usuario.email}'
+            )">
+                Editar
+            </button>
+            <button onclick="eliminarUsuario('${usuario.id}')">
+                Eliminar
+            </button>
+        `;
         container.appendChild(card);
     });
 }
@@ -34,20 +59,69 @@ form.addEventListener("submit", async (e) => {
     e.preventDefault();
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
-    const { data, error } = await supabaseClient
-        .from("usuarios")
-        .insert([
-            {
+    let response;
+
+    if (editandoId !== null) {
+        response = await supabaseClient
+            .from("usuarios")
+            .update({
                 name: name,
                 email: email
-            }
-        ]);
-    if (error) {
-        console.log("Error:", error);
+
+            })
+            .eq("id", editandoId);
     } else {
-        console.log("Usuario creado:", data);
-        alert("Usuario creado correctamente");
-        form.reset();
+        response = await supabaseClient
+            .from("usuarios")
+            .insert([{
+                name: name,
+                email: email
+            }]);
+    }
+
+    if (response.error) {
+        console.log("Error:", response.error);
+    } else {
+        alert(
+            editandoId
+                ? "Usuario actualizado"
+                : "Usuario creado"
+        );
+        document
+            .getElementById("userForm")
+            .reset();
+        editandoId = null;
+        document
+            .getElementById("saveButton")
+            .textContent = "Crear usuario";
         cargarUsuarios();
     }
 });
+
+window.editarUsuario = function (id, name, email) {
+    console.log("Editando:", id, name, email);
+    editandoId = id;
+    document.getElementById("name").value = name;
+    document.getElementById("email").value = email;
+    document.getElementById("saveButton").textContent = "Actualizar usuario";
+};
+cargarUsuarios();
+
+window.eliminarUsuario = async function(id) {
+    const confirmar = confirm(
+        "¿Estás seguro de eliminar este usuario?"
+    );
+    if(!confirmar){
+        return;
+    }
+    const { error } = await supabaseClient
+        .from("usuarios")
+        .delete()
+        .eq("id", id);
+    if(error){
+        console.log("Error:", error);
+    } else {
+        alert("Usuario eliminado");
+        cargarUsuarios();
+    }
+};
